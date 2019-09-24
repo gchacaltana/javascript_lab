d73f0aea6e62439b15b3e0983bf7a3c08d6871bd
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2019-09-24 13:13:28 -0500

    Example with Object.fromEntries

---
+++ Object.fromEntries.js
@@ -0,0 +1,13 @@
+let twitter = {
+    name: "Gonzalo Chacaltana",
+    location: "Perú",
+    account: "@gchacaltanab"
+};
+console.log(Object.entries(twitter));
+/* Expected Output
+ [
+ ["name", "Gonzalo Chacaltana"],
+ ["location", "Perú"],
+ ["account", "@gchacaltanab"]
+ ]
+ */
